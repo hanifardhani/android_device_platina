1f29816e68efa1a247c94380b523d8135e1b6328
Rique Barros <henripaz1998@gmail.com>
2020-04-12 22:11:39 -0300

    platina: Add display cutout makefiles
    Signed-off-by: Rique Barros <henripaz1998@gmail.com>

--- device.mk
+++ device.mk
@@ -319,6 +319,11 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     libandroid_net \
     netutils-wrapper-1.0
+    
+# Notch
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    NotchBarKiller
 
 # OMX
 PRODUCT_PACKAGES += \
